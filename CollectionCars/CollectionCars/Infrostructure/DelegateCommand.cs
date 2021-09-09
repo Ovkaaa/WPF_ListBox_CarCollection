00f2f023235601d29c4836e82bce8eb8dac8d2cb
Ovkaaa <79980872+Ovkaaa@users.noreply.github.com>
2021-09-09 16:38:32 +0300

    Add files via upload

---
+++ CollectionCars/CollectionCars/Infrostructure/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CollectionCars.Infrostructure
+{
+    public class DelegateCommand : ICommand
+    {
+        private Func<object, bool> canExecuteMethod;
+        private Action<object> executeMethod;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DelegateCommand(Action<object> eM, Func<object, bool> cEM = null)
+        {
+            executeMethod = eM;
+            canExecuteMethod = cEM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteMethod == null || canExecuteMethod(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            executeMethod(parameter);
+        }
+    }
+}
